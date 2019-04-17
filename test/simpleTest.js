21728dcffc2e672404a1fed41be3778f41d059d0
Panu Ausavasereelert <panu@panu.in.th>
2019-04-17 12:00:18 +0700

    Updated unit test

--- test/simpleTest.js
+++ test/simpleTest.js
@@ -1,9 +1,9 @@
 var assert = require('assert');
-describe('Basic Mocha String Test', function () {
- it('should return number of charachters in a string', function () {
-        assert.equal("Hello".length, 5);
+describe('Basic unit test', function () {
+    it('should return number of characters in a string', function () {
+        assert.equal("Demonstration".length, 13);
     });
- it('should return first charachter of the string', function () {
-        assert.equal("Hello".charAt(0), 'H');
+    it('should return first charachter of the string', function () {
+        assert.equal("Demonstration".charAt(0), 'D');
     });
 });
